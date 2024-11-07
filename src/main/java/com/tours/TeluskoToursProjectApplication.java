e9811f7485ebf566bf0dfa455bedfa0cad46af62
muskangarg03 <muskangarg676@gmail.com>
2024-11-07 18:21:12 +0530

    log clears

--- src/main/java/com/tours/TeluskoToursProjectApplication.java
+++ src/main/java/com/tours/TeluskoToursProjectApplication.java
@@ -3,12 +3,28 @@ package com.tours;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 @SpringBootApplication
 public class TeluskoToursProjectApplication {
 
 	public static void main(String[] args) {
+		clearLogFile("C://Users//DELL//Downloads//TeluskoToursProject//TeluskoToursProject//src//main//java//com//tours//Logging//app.log");
 		SpringApplication.run(TeluskoToursProjectApplication.class, args);
 		System.out.println("Hello....git ..");
 	}
 
+	private static void clearLogFile(String logFilePath) {
+		try (FileWriter fileWriter = new FileWriter(logFilePath, false)) {
+			fileWriter.write(""); // Clear contents
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
+
+
+
+
+
