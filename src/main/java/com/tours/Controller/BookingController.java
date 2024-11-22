dcf9bee8d6f8bb4f64afa1742d0af7992e4a5f00
muskangarg03 <muskangarg676@gmail.com>
2024-11-22 10:09:54 +0530

    cors

--- src/main/java/com/tours/Controller/BookingController.java
+++ src/main/java/com/tours/Controller/BookingController.java
@@ -16,6 +16,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/customer")
+@CrossOrigin(origins = "*")
 public class BookingController {
 
     @Autowired
--- src/main/java/com/tours/Controller/UserController.java
+++ src/main/java/com/tours/Controller/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin(origins = "*")
 public class UserController {
 
     @Autowired
